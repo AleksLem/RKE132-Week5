0568eafbc284d73e6ae5fc3ce3ea5473d6d8c461
Aleks Lemming <aleks.lemming@tktk.ee>
2023-03-02 11:09:09 +0200

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false 
+//fasle && false --> false
+
+//OR - || (PIPES)
+//true || true --> true
+//true || false --> true
+//false || true --> true
+// false || false --> false
+
